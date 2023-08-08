4ce466090df148a3a15c302f493151cdc3249a8d
liwang.pu@going-link.com <liwang.pu@going-link.com>
2023-08-08 23:53:42 +0800

    添加文件过滤快捷选择

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,16 +39,19 @@
             this.btnSelectDest = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.txtFileFilter = new System.Windows.Forms.TextBox();
+            this.btnPrefixGenerator = new System.Windows.Forms.Button();
+            this.btnFileSelectOfAllFile = new System.Windows.Forms.Button();
+            this.btnFileSelectOfMP4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 35);
+            this.label1.Location = new System.Drawing.Point(16, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.Size = new System.Drawing.Size(59, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "源文件:";
+            this.label1.Text = "源文件夹:";
             // 
             // txtSource
             // 
@@ -71,7 +74,7 @@
             // 
             this.txtPrefix.Location = new System.Drawing.Point(81, 81);
             this.txtPrefix.Name = "txtPrefix";
-            this.txtPrefix.Size = new System.Drawing.Size(283, 23);
+            this.txtPrefix.Size = new System.Drawing.Size(202, 23);
             this.txtPrefix.TabIndex = 1;
             // 
             // btnSelectSource
@@ -87,9 +90,9 @@
             // btnHandle
             // 
             this.btnHandle.Enabled = false;
-            this.btnHandle.Location = new System.Drawing.Point(289, 215);
+            this.btnHandle.Location = new System.Drawing.Point(289, 238);
             this.btnHandle.Name = "btnHandle";
-            this.btnHandle.Size = new System.Drawing.Size(75, 23);
+            this.btnHandle.Size = new System.Drawing.Size(75, 31);
             this.btnHandle.TabIndex = 3;
             this.btnHandle.Text = "开始处理";
             this.btnHandle.UseVisualStyleBackColor = true;
@@ -98,23 +101,24 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 173);
+            this.label3.Location = new System.Drawing.Point(4, 196);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.Size = new System.Drawing.Size(71, 17);
             this.label3.TabIndex = 0;
-            this.label3.Text = "源文件:";
+            this.label3.Text = "目标文件夹:";
             // 
             // txtDest
             // 
             this.txtDest.Enabled = false;
-            this.txtDest.Location = new System.Drawing.Point(81, 170);
+            this.txtDest.Location = new System.Drawing.Point(81, 193);
             this.txtDest.Name = "txtDest";
+            this.txtDest.PlaceholderText = "默认同源文件夹";
             this.txtDest.Size = new System.Drawing.Size(202, 23);
             this.txtDest.TabIndex = 1;
             // 
             // btnSelectDest
             // 
-            this.btnSelectDest.Location = new System.Drawing.Point(289, 170);
+            this.btnSelectDest.Location = new System.Drawing.Point(289, 193);
             this.btnSelectDest.Name = "btnSelectDest";
             this.btnSelectDest.Size = new System.Drawing.Size(75, 23);
             this.btnSelectDest.TabIndex = 2;
@@ -137,13 +141,46 @@
             this.txtFileFilter.Name = "txtFileFilter";
             this.txtFileFilter.Size = new System.Drawing.Size(283, 23);
             this.txtFileFilter.TabIndex = 1;
-            this.txtFileFilter.Text = "*.mp4";
+            this.txtFileFilter.Text = "*.*";
+            // 
+            // btnPrefixGenerator
+            // 
+            this.btnPrefixGenerator.Location = new System.Drawing.Point(289, 81);
+            this.btnPrefixGenerator.Name = "btnPrefixGenerator";
+            this.btnPrefixGenerator.Size = new System.Drawing.Size(75, 23);
+            this.btnPrefixGenerator.TabIndex = 4;
+            this.btnPrefixGenerator.Text = "随机前缀";
+            this.btnPrefixGenerator.UseVisualStyleBackColor = true;
+            this.btnPrefixGenerator.Click += new System.EventHandler(this.btnPrefixGenerator_Click);
+            // 
+            // btnFileSelectOfAllFile
+            // 
+            this.btnFileSelectOfAllFile.Location = new System.Drawing.Point(80, 155);
+            this.btnFileSelectOfAllFile.Name = "btnFileSelectOfAllFile";
+            this.btnFileSelectOfAllFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelectOfAllFile.TabIndex = 5;
+            this.btnFileSelectOfAllFile.Text = "所有文件";
+            this.btnFileSelectOfAllFile.UseVisualStyleBackColor = true;
+            this.btnFileSelectOfAllFile.Click += new System.EventHandler(this.btnFileSelectOfAllFile_Click);
+            // 
+            // btnFileSelectOfMP4
+            // 
+            this.btnFileSelectOfMP4.Location = new System.Drawing.Point(161, 155);
+            this.btnFileSelectOfMP4.Name = "btnFileSelectOfMP4";
+            this.btnFileSelectOfMP4.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelectOfMP4.TabIndex = 5;
+            this.btnFileSelectOfMP4.Text = "MP4";
+            this.btnFileSelectOfMP4.UseVisualStyleBackColor = true;
+            this.btnFileSelectOfMP4.Click += new System.EventHandler(this.btnFileSelectOfMP4_Click);
             // 
             // FileRenameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.ClientSize = new System.Drawing.Size(384, 281);
+            this.Controls.Add(this.btnFileSelectOfMP4);
+            this.Controls.Add(this.btnFileSelectOfAllFile);
+            this.Controls.Add(this.btnPrefixGenerator);
             this.Controls.Add(this.btnHandle);
             this.Controls.Add(this.btnSelectDest);
             this.Controls.Add(this.btnSelectSource);
@@ -156,8 +193,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(400, 300);
-            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.MaximumSize = new System.Drawing.Size(400, 320);
+            this.MinimumSize = new System.Drawing.Size(400, 320);
             this.Name = "FileRenameForm";
             this.Text = "文件更名";
             this.Load += new System.EventHandler(this.FileRenameForm_Load);
@@ -179,5 +216,8 @@
         private Button btnSelectDest;
         private Label label4;
         private TextBox txtFileFilter;
+        private Button btnPrefixGenerator;
+        private Button btnFileSelectOfAllFile;
+        private Button btnFileSelectOfMP4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,26 +1,40 @@
+using shortid;
+using shortid.Configuration;
+
 namespace FileRename
 {
     public partial class FileRenameForm : Form
     {
+        private GenerationOptions shortIdOptions;
         public FileRenameForm()
         {
             InitializeComponent();
         }
 
+
         private void FileRenameForm_Load(object sender, EventArgs e)
         {
-            this.ActiveControl = btnSelectSource;
+            ActiveControl = btnSelectSource;
+            shortIdOptions = new GenerationOptions(false, false, 8);
+            generateRandomPrefix();
         }
+
+        private void generateRandomPrefix()
+        {
+            txtPrefix.Text = ShortId.Generate(new GenerationOptions(false, false, 8)).ToUpper();
+        }
+
 
         private void btnHandle_Click(object sender, EventArgs e)
         {
             var filter = string.IsNullOrWhiteSpace(txtFileFilter.Text) ? "*.*" : txtFileFilter.Text;
             var files = Directory.GetFiles(txtSource.Text, filter, SearchOption.AllDirectories);
             var destFolder = string.IsNullOrWhiteSpace(txtDest.Text) ? txtSource.Text : txtDest.Text;
+
             foreach (var file in files)
             {
                 var ext = Path.GetExtension(file);
-                var newFileName = $"{Guid.NewGuid().ToString("N").ToUpper()}{ext}";
+                var newFileName = $"{ ShortId.Generate(shortIdOptions)}{ext}";
                 if (!string.IsNullOrWhiteSpace(txtPrefix.Text))
                 {
                     newFileName = $"{txtPrefix.Text}-{newFileName}";
@@ -62,5 +76,20 @@ namespace FileRename
             }
         }
 
+        private void btnPrefixGenerator_Click(object sender, EventArgs e)
+        {
+            generateRandomPrefix();
+            btnHandle.Enabled = true;
+        }
+
+        private void btnFileSelectOfAllFile_Click(object sender, EventArgs e)
+        {
+            txtFileFilter.Text = "*.*";
+        }
+
+        private void btnFileSelectOfMP4_Click(object sender, EventArgs e)
+        {
+            txtFileFilter.Text = "*.mp4";
+        }
     }
 }
